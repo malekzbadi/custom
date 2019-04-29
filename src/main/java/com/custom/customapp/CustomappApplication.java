bd350945cd6c4c2442d6bc5217529b3ae5893241
Zbadi <malek.zbadi@accenture.com>
2019-04-29 17:17:30 +0200

    adding html & css

--- src/main/java/com/custom/customapp/CustomappApplication.java
+++ src/main/java/com/custom/customapp/CustomappApplication.java
@@ -1,20 +1,10 @@
 package com.custom.customapp;
 
-import jdk.nashorn.internal.ir.annotations.Reference;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class CustomappApplication {
-
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String message(){
-        return "Hello world - what a cruel world!!";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(CustomappApplication.class, args);
